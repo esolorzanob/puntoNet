1cf76beb0b03e623dd13811f2549ac23f6ba0e27
esolorzanob <esolorzano@renovatiocloud.com>
2017-05-22 09:40:00 -0600

    actualizado

---
+++ miPrimerC/miPrimerC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miPrimerC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            for (var i=0; i < 5; i++) {
+                lista.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine(lista[0]);
+            Console.WriteLine(String.Join(",", lista));
+            Console.WriteLine("Presione cualquier tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
